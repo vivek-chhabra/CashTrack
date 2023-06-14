789e7345dc78589c61309ddd0821f400ed646101
vivek chhabra <vivekcb8642@gmail.com>
2023-06-14 12:39:48 +0530

    add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import '../components/NavBar.css'
+
+export default function NavBar() {
+    return (
+        <div className="NavBar">
+            <nav className="container">
+                <NavLink to={"/"} className={"brand"}>
+                    cashTrack
+                </NavLink>
+                <div className="links">
+                    <NavLink to={"/login"} className={'link'}>Login</NavLink>
+                    <NavLink to={"/signup"} className={'link'}>SignUp</NavLink>
+                </div>
+            </nav>
+        </div>
+    );
+}
