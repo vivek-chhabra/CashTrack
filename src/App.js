54251f3c47996a779ed4078ee00bd785ce323000
vivek chhabra <vivekcb8642@gmail.com>
2023-06-14 16:18:13 +0530

    add routes to navigate to different paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./pages/signup/SignUp";
 import NavBar from "./components/NavBar";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
         </div>
     );
 }
