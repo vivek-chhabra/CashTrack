6fe151ee0937234e281f5df3610d3e7b62d5ff85
vivek chhabra <vivekcb8642@gmail.com>
2023-06-15 11:18:31 +0530

    update Login and SignUp component

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,37 +1,59 @@
-import React from "react";
+import { useInput } from "../../hooks/useInput";
+import React, { useState } from "react";
 import "../login/Login.css";
 
 export default function Login() {
+    // input hook
+    const [email, updateEmail, resetEmail] = useInput("");
+    const [password, updatePassword, resetPassword] = useInput("");
+
+    // state for error
+    const [error, setError] = useState("");
+
+    // login with email and password
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    // login with google
+   
+
     return (
         <div className="Login">
             <div className="container">
-                <h2>Login</h2>
-                <form>
-                    <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">
-                            Email address :
-                        </label>
-                        <input type="email" className="shadow-none form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                    <div className="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">
-                            Password :
-                        </label>
-                        <input type="password" className="shadow-none form-control" id="exampleInputPassword1" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">
-                        Submit
-                    </button>
-                </form>
-                <div className="separate">
-                    <div className="line"></div>
-                    <div className="or">OR</div>
-                    <div className="line"></div>
-                </div>
-                <button type="submit" className="loginWithGoogle">
-                    <img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png" alt="" />
-                    <p>Login With Google</p>
-                </button>
+                {error ? (
+                    <></>
+                ) : (
+                    <>
+                        <h2>Login</h2>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <label for="exampleInputEmail1" className="form-label">
+                                    Email address :
+                                </label>
+                                <input type="email" required value={email} onChange={updateEmail} className="shadow-none form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            </div>
+                            <div className="mb-3">
+                                <label for="exampleInputPassword1" className="form-label">
+                                    Password :
+                                </label>
+                                <input type="password" required value={password} onChange={updatePassword} className="shadow-none form-control" id="exampleInputPassword1" />
+                            </div>
+                            <button type="submit" className="btn btn-primary">
+                                Login
+                            </button>
+                        </form>
+                        <div className="separate">
+                            <div className="line"></div>
+                            <div className="or">OR</div>
+                            <div className="line"></div>
+                        </div>
+                        <button type="submit" className="loginWithGoogle">
+                            <img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png" alt="" />
+                            <p>Login With Google</p>
+                        </button>
+                    </>
+                )}
             </div>
         </div>
     );
--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../login/Login.css";
 import { useInput } from "../../hooks/useInput";
 import { useSignUpAuth } from "../../hooks/useSignUpAuth";
+import { ErrorMsg } from "../../helpers";
+import { AuthContext } from "../../context/AuthContext";
+import { useToggle } from "../../hooks/useToggle";
+import { NavLink } from "react-router-dom";
 
 export default function SignUp() {
     // input hook
@@ -10,54 +14,89 @@ export default function SignUp() {
     const [email, updateEmail, resetEmail] = useInput("");
     const [password, updatePassword, resetPassword] = useInput("");
 
+    // toggle hook
+    const [isHidden, toggleIsHidden] = useToggle(true);
+
+    // consuming the auth context
+    const { user } = useContext(AuthContext);
+
     // sign up using hook
-    const { error, signUp } = useSignUpAuth(firstName, lastName, email, password);
+    const { error, signUp, isPending } = useSignUpAuth(firstName, lastName, email, password);
 
     // handling submit
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        signUp();
-        console.log(error)
-        // resetEmail();
-        // resetFName();
-        // resetLName();
-        // resetPassword();
+        await signUp();
+        if (error === null) {
+            resetEmail();
+            resetFName();
+            resetLName();
+            resetPassword();
+        }
     };
+    if (error === null) {
+        resetEmail();
+        resetFName();
+        resetLName();
+        resetPassword();
+    }
 
     return (
-        <div className="SignUp Login">
-            <div className="container">
-                <h2>Sign Up</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label for="firstName" className="form-label">
-                            First Name :
-                        </label>
-                        <input type="text" required value={firstName} onChange={updateFirstName} className="shadow-none form-control" id="firstName" aria-describedby="emailHelp" />
-                    </div>
-                    <div className="mb-3">
-                        <label for="lastName" className="form-label">
-                            Last Name :
-                        </label>
-                        <input type="text" required value={lastName} onChange={updateLastName} className="shadow-none form-control" id="lastName" aria-describedby="emailHelp" />
-                    </div>
-                    <div className="mb-3">
-                        <label for="email" className="form-label">
-                            Email address :
-                        </label>
-                        <input type="email" required value={email} onChange={updateEmail} className="shadow-none form-control" id="email" aria-describedby="emailHelp" />
-                    </div>
-                    <div className="mb-3">
-                        <label for="password" className="form-label">
-                            Create Password :
-                        </label>
-                        <input type="password" required value={password} onChange={updatePassword} className="shadow-none form-control" id="password" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">
-                        Create Account
-                    </button>
-                </form>
+        <>
+            {error && <ErrorMsg error={error} />}
+            <div className="SignUp Login">
+                <div className="container">
+                    <h2>Sign Up</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-3 name-section">
+                            <label for="firstName" className="form-label">
+                                Name :
+                            </label>
+                            <div className="name-input">
+                                <input type="text" placeholder="First Name" required value={firstName} onChange={updateFirstName} className="shadow-none form-control" id="firstName" aria-describedby="emailHelp" />
+                                <input type="text" placeholder="Last Name" required value={lastName} onChange={updateLastName} className="shadow-none form-control" id="lastName" aria-describedby="emailHelp" />
+                            </div>
+                        </div>
+                        <div className="mb-3">
+                            <label for="email" className="form-label">
+                                Email address :
+                            </label>
+                            <input type="email" placeholder="Your Email" required value={email} onChange={updateEmail} className="shadow-none form-control" id="email" aria-describedby="emailHelp" />
+                        </div>
+                        <div className="mb-3">
+                            <label for="password" className="form-label">
+                                Create Password :
+                            </label>
+                            <input type={isHidden ? "password" : "text"} placeholder="Make a Strong Password" required value={password} onChange={updatePassword} className="shadow-none form-control" id="password" />
+                        </div>
+                        <div className="mb-3 form-check">
+                            <input type="checkbox" className="form-check-input shadow-none" onClick={toggleIsHidden} id="exampleCheck1" />
+                            <label className="form-check-label" for="exampleCheck1">
+                                See Password
+                            </label>
+                        </div>
+                        <div className="sepr"></div>
+                        <div className="have-an-acc">
+                            <p>
+                                Already have an account ?{" "}
+                                <NavLink to={"/login"} className={"loginEither"}>
+                                    Log in
+                                </NavLink>
+                            </p>
+                        </div>
+                        <div className="sepr"></div>
+                        {isPending ? (
+                            <button type="submit" disabled className="mt-3 btn btn-primary">
+                                Loading...
+                            </button>
+                        ) : (
+                            <button type="submit" className="mt-3 btn btn-primary">
+                                Create Account
+                            </button>
+                        )}
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
