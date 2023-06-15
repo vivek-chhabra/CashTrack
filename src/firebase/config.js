44872d66916fdcd0431a3a523de0f78c1c119e99
vivek chhabra <vivekcb8642@gmail.com>
2023-06-15 11:20:51 +0530

    update firebase config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -15,4 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = new getFirestore(app);
+export const db =  getFirestore(app);
+export const googleProvider = new GoogleAuthProvider()
