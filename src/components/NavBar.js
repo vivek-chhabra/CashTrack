993d34b7ac9f8cf3728fa80969dc21c1a85aff21
vivek chhabra <vivekcb8642@gmail.com>
2023-06-17 15:44:04 +0530

    update miscellaneous files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import '../components/NavBar.css'
+import "../components/NavBar.css";
+import { useLogOut } from "../hooks/useLogOut";
+import { AuthContext } from "../context/AuthContext";
+import { auth } from "../firebase/config";
+import { capitalize } from "../helpers";
 
 export default function NavBar() {
+    // using log out hook
+    const { error, isPending, logOut } = useLogOut();
+
+    // consuming auth context
+    const { user } = useContext(AuthContext);
+
+
     return (
         <div className="NavBar">
             <nav className="container">
@@ -10,8 +21,23 @@ export default function NavBar() {
                     cashTrack
                 </NavLink>
                 <div className="links">
-                    <NavLink to={"/login"}  className={'link'}>Login</NavLink>
-                    <NavLink to={"/signup"}  className={'link'}>SignUp</NavLink>
+                    {!user ? (
+                        <>
+                            <NavLink to={"/login"} className={"link"}>
+                                Login
+                            </NavLink>
+                            <NavLink to={"/signup"} className={"link"}>
+                                SignUp
+                            </NavLink>
+                        </>
+                    ) : (
+                        <>
+                            <p className="username">Hello, {user.displayName.split(" ")[0]}</p>
+                            <NavLink to={"/login"} className={"link btn btn-success sign-out"} onClick={logOut}>
+                                Sign Out
+                            </NavLink>
+                        </>
+                    )}
                 </div>
             </nav>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
 import "./index.css";
+import AuthProvider from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <AuthProvider>
+                <App />
+            </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
